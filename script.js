5b12856fa585f26ea31d605ee0a7cba7051c9e1a
Abanum Olise <abanum00@gmail.com>
2022-07-25 12:53:29 +0100

    all done

--- script.js
+++ script.js
@@ -108,3 +108,11 @@ let createLinkBox = (searchTerm, key) => {
         form.after(resultContainer);
     }
 }
+
+
+let menuBtn = document.querySelector(".menu_btn");
+let navCard = document.querySelector(".nav_card");
+
+menuBtn.addEventListener("click", () => {
+    navCard.classList.toggle("hide");
+})
